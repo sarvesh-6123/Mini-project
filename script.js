12fdcf09a47994126061df62b2326220a01169cf
sarvesh-6123 <sarveshvaraams6123@gmail.com>
2024-04-07 21:37:11 +0530

    message

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function generatePassword() {
-    var length = 10;
+    var length = 12;
     var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[]:;<>,.?/";
     var password = "";
     for (var i = 0; i < length; i++) {
         var charIndex = Math.floor(Math.random() * charset.length);
         password += charset.charAt(charIndex);
     }
-    document.getElementById("passwordDisplay").textContent = password;
+    let display = document.getElementById("passwordDisplay");
+    if(display.innerHTML != "Your Password Will Appear Here"){
+        alert("Password already generated");
+    }
+    else{
+          display.textContent = password;
+    }
 }
