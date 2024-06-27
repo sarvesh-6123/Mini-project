2d6162fda3755c366243a66d95cae4aa88c1281c
Sarvesh Varaa <113160348+sarvesh-6123@users.noreply.github.com>
2024-06-27 19:18:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+function makeItGood(pass) {
+    var result = "";
+    for (let x of pass) {
+        if (x >= 'A' && x <= 'Z') result += x;
+    }
+    for (let x of pass) {
+        if (x >= 'a' && x <= 'z') result += x;
+    }
+    for (let x of pass) {
+        if (x >= '0' && x <= '9') result += x;
+    }
+    for (let x of pass) {
+        if (!(x >= 'A' && x <= 'Z') && !(x >= 'a' && x <= 'z') && !(x >= '0' && x <= '9')) result += x;
+    }
+    return result;
+}
 function generatePassword() {
     var length = 12;
-    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[]:;<>,.?/";
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*_+<>,.?";
     var password = "";
     for (var i = 0; i < length; i++) {
         var charIndex = Math.floor(Math.random() * charset.length);
         password += charset.charAt(charIndex);
     }
     let display = document.getElementById("passwordDisplay");
-    if(display.innerHTML != "Your Password Will Appear Here"){
-        alert("Password already generated");
-    }
-    else{
-          display.textContent = password;
-    }
+    let head = document.getElementById("head");
+    password = makeItGood(password);
+    display.textContent = password;
+    head.textContent = "Your Password can be..!"
 }
